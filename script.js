adf6251999dd1782ac57968cb4d97fb0ad37abb6
Tu-Amigooo <chatterjeeanshm@gmail.com>
2025-01-07 17:09:47 +0530

    Update script.js
    new message

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 let sendThis = `Thanks Anku, for everything... Pata nahi kitne baar kitne tarike se tumhe thanks khe sakta hu, par honestly thanks mere life me aane ke liye, mujhe apna bana ne ke liye. Tumhare sath bitaya harr ek pal mere liye ANMOL rha hai, thanks mere chere ki muskan ban ne ke liye, thanks mere harr gumho ko samjhne ke liye. Log to pyaar me Taj Mahal bana dete... But me itna hi bana sakta hu (hehe), so here is the my first ever created webpage, dedicated only to you....My Nezu.` 
-
+let sendAfternoon = `Thanks Nezu, for being the brightest sun ray that lighten my like... I wish to live that much joyfull life with you my sunshine !`
+let sendEvening = ` Never get fade out of my sight, Tumhare sath bityai har sham mere liye best rahi hai and i wish humesha rahe, Love you Anku.`
 let sendThat = `I Love You Alwways`
 
 function send(){
@@ -16,10 +17,12 @@ function doMes(){
         alert(`${sendThis}`);
     }
     else if (hours > 12 && hours < 18){
-        alert(`Good Afternoon My Love`);
+        alert(`Good Afternoon My Love 
+        ${sendAfternoon}`);
     }
     else if (hours > 18 && hours < 20){
-        alert(`Good Evening Nezu`);
+        alert(`Good Evening Nezu 
+        ${sendEvening}`);
     }
     else{
         alert(`${sendThat}`)
